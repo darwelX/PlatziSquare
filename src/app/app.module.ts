8415630db35d298c3f24dc372d2bef184d268e70
Darwel Quintero <darwel34@gmail.com>
2017-12-13 17:47:06 -0430

    se comentaron para que sire cada seccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
+  //en este array se agregan los componentes propios que conforman el modulo
   declarations: [
     AppComponent
   ],
+
+  //se importa los modulas nativos de angular que va a necesitar la aplicacion
   imports: [
     BrowserModule
   ],
+
+  //se declaran los servicios propios que se van a necesitar en la aplicacion
   providers: [],
+
+  //se declara con que componente se va a iniciar
   bootstrap: [AppComponent]
 })
 export class AppModule { }
