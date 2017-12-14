b494a5a0493e704915e657699f77de90c1379ac0
Darwel Quintero <darwel34@gmail.com>
2017-12-14 11:08:31 -0430

    se añadio inicializacion y funciones de comportamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PlatziSquare';
+  listo = true;
+  boton = 'btn';
+  contador = 0;
+  nombre="";
+  genero = "Mr";
+  constructor(){
+    setTimeout(()=>{
+      this.listo = false;
+    }, 3000)
+  }
+
+  sumar(){
+    this.contador++;
+  }
+
+  set(event){
+    console.log('hola');
+    this.contador = event.target.value;
+  }
 }
