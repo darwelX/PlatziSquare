95ae4792218f8b1770c888453c63f397eae8a4c6
Darwel Quintero <darwel34@gmail.com>
2017-12-18 19:17:29 -0430

    se re acomodo los datos iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PlatziSquare';
-  listo = true;
-  boton = 'btn';
-  contador = 0;
-  nombre="";
-  genero = "Mr";
-  constructor(){
-    setTimeout(()=>{
-      this.listo = false;
-    }, 3000)
-  }
+  lat:number = 8.215178;
+  lng:number = -72.257524;
 
-  sumar(){
-    this.contador++;
-  }
-
-  set(event){
-    console.log('hola');
-    this.contador = event.target.value;
-  }
+  lugares = [
+    {plan: 'pagado', activo: true, cercania:1 , distancia: 1, nombre: "Pizzeria y Hamburguesas Don Pedrito"},
+    {plan: 'gratuito', activo: false,cercania:2 , distancia: 5, nombre: "Veterinaria Kaninos"},
+    {plan: 'gratuito', activo: true, cercania:3 , distancia: 110, nombre: "Restaurant la Cotorra"},
+    {plan: 'gratuito', activo: true, cercania:1 , distancia: 2, nombre: "El Universo del Churro"},
+  ]
 }
