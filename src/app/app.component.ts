ac3efc9fd18287219aa18f9c8c8ab1e6595f3900
Darwel Quintero <darwel34@gmail.com>
2017-12-20 04:11:30 -0430

    se quitaron los datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PlatziSquare';
-  lat:number = 8.215178;
-  lng:number = -72.257524;
-
-  lugares = [
-    {plan: 'pagado', activo: true, cercania:1 , distancia: 1, nombre: "Pizzeria y Hamburguesas Don Pedrito"},
-    {plan: 'gratuito', activo: false,cercania:2 , distancia: 5, nombre: "Veterinaria Kaninos"},
-    {plan: 'gratuito', activo: true, cercania:3 , distancia: 110, nombre: "Restaurant la Cotorra"},
-    {plan: 'gratuito', activo: true, cercania:1 , distancia: 2, nombre: "El Universo del Churro"},
-  ]
 }
