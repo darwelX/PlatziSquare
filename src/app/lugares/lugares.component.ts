635eff3ed8cc769b5d44974b115a5d6e6ee626d0
Darwel Quintero <darwel34@gmail.com>
2017-12-20 08:36:19 -0430

    se agrego id a los objetos

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -10,10 +10,10 @@ export class LugaresComponent implements OnInit {
   lng:number = -72.257524;
 
   lugares = [
-    {plan: 'pagado', activo: true, cercania:1 , distancia: 1, nombre: "Pizzeria y Hamburguesas Don Pedrito"},
-    {plan: 'gratuito', activo: false,cercania:2 , distancia: 5, nombre: "Veterinaria Kaninos"},
-    {plan: 'gratuito', activo: true, cercania:3 , distancia: 110, nombre: "Restaurant la Cotorra"},
-    {plan: 'gratuito', activo: true, cercania:1 , distancia: 2, nombre: "El Universo del Churro"},
+    {id: 1, plan: 'pagado', activo: true, cercania:1 , distancia: 1, nombre: "Pizzeria y Hamburguesas Don Pedrito"},
+    {id: 2, plan: 'gratuito', activo: false,cercania:2 , distancia: 5, nombre: "Veterinaria Kaninos"},
+    {id: 3, plan: 'gratuito', activo: true, cercania:3 , distancia: 110, nombre: "Restaurant la Cotorra"},
+    {id: 4, plan: 'gratuito', activo: true, cercania:1 , distancia: 2, nombre: "El Universo del Churro"},
   ]
   constructor() { }
 
