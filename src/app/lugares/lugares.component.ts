0043cfc004643ef5b5a3b8cd3d583e287ca9add3
Darwel Quintero <darwel34@gmail.com>
2017-12-22 21:17:51 -0430

    se añadio el servicio pra consultar los lugares

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,3 +1,4 @@
+import { LugaresService } from '../services/lugares.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class LugaresComponent implements OnInit {
   lat:number = 8.215178;
   lng:number = -72.257524;
-
-  lugares = [
-    {id: 1, plan: 'pagado', activo: true, cercania:1 , distancia: 1, nombre: "Pizzeria y Hamburguesas Don Pedrito"},
-    {id: 2, plan: 'gratuito', activo: false,cercania:2 , distancia: 5, nombre: "Veterinaria Kaninos"},
-    {id: 3, plan: 'gratuito', activo: true, cercania:3 , distancia: 110, nombre: "Restaurant la Cotorra"},
-    {id: 4, plan: 'gratuito', activo: true, cercania:1 , distancia: 2, nombre: "El Universo del Churro"},
-  ]
-  constructor() { }
+  lugares: any = null;
+  constructor(private lugaresServices: LugaresService) { 
+    this.lugares = lugaresServices.getLugares();
+  }
 
   ngOnInit() {
   }
