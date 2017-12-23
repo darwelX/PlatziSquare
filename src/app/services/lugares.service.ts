6457f1f44661e3a9ed6cada7c70a870af32ea582
Darwel Quintero <darwel34@gmail.com>
2017-12-23 16:02:34 -0430

    se inyecto en el constructor el listado de lugares

--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from'angularfire2/database';
 @Injectable()
 export class LugaresService {
-
+  lugares: any = [];
   public getLugares(){
     return this.afDB.list('lugares/');
   }
 
-  public buscarLugar(id, lugares){
-    return lugares.filter((lugar)=>{return lugar.id == id})[0] || null;
+  public buscarLugar(id){
+    return this.lugares.filter((lugar)=>{return lugar.id == id})[0] || null;
   }
 
   public guardarLugar(lugar){
     // console.log('guardando',lugar);
     this.afDB.database.ref('lugares/'+lugar.id).set(lugar);
   }
-  constructor(private afDB: AngularFireDatabase) { }
+  constructor(private afDB: AngularFireDatabase) { 
+    this.getLugares()
+    .valueChanges().subscribe(lugares => {
+      // console.log('aqui',lugares);
+      this.lugares = lugares;
+    });
+  }
 
 }
