a309c4f12b2caa57f3059f2fe682086362847ead
Darwel Quintero <darwel34@gmail.com>
2017-12-22 21:17:12 -0430

    se refactorizo el codigo para que acepte un servicio en lugar de una variable estatica para consultar los lugares

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,3 +1,4 @@
+import { LugaresService } from '../services/lugares.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -9,24 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 export class DetalleComponent implements OnInit {
   id = null;
   lugar: any = {};
-  lugares = [
-    {id: 1, plan: 'pagado', activo: true, cercania:1 , distancia: 1, nombre: "Pizzeria y Hamburguesas Don Pedrito", descripcion: 'Las mejores pizzas hechas a la leña con el mejor sabor y variedad'},
-    {id: 2, plan: 'gratuito', activo: false,cercania:2 , distancia: 5, nombre: "Veterinaria Kaninos", descripcion: 'Consiente a tu mascota para ello tenemos espa, baños especiales y de mas atenciones'},
-    {id: 3, plan: 'gratuito', activo: true, cercania:3 , distancia: 110, nombre: "Restaurant la Cotorra", descripcion: 'Los mejores almuerzos con platillos espciales a la carta los mas sabrosos de la ciudad'},
-    {id: 4, plan: 'gratuito', activo: true, cercania:1 , distancia: 2, nombre: "El Universo del Churro", descripcion: 'El universo del churro te ofrece un mundo de sabores con la mejor bariedad de acompañantes y bebidas'},
-  ]
+  lugares: any = null;
   //en la clase ActivateRoute se resiven parametros
-  constructor(private route: ActivatedRoute) { 
+  constructor(private route: ActivatedRoute, private lugaresServices: LugaresService) { 
+    this.lugares = lugaresServices.getLugares();
     console.log(this.route.snapshot.params['id']);
     console.log(this.route.snapshot.queryParams['action']);
     this.id = this.route.snapshot.params['id'];
-    this.lugar = this.buscarLugar();
+    this.lugar = lugaresServices.buscarLugar(this.id);
   }
 
   ngOnInit() {
-  }
-
-  buscarLugar(){
-    return this.lugares.filter((lugar)=>{return lugar.id == this.id})[0] || null;
   }
 }
