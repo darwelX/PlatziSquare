df2da3df03cecbde8a41590b5eec0aed1eaddbdc
Darwel Quintero <darwel34@gmail.com>
2017-12-22 21:15:24 -0430

    se añadio el servicio de lugares

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {Routes, RouterModule} from '@angular/router';
 import { DetalleComponent } from './detalle/detalle.component';
 import { LugaresComponent } from './lugares/lugares.component';
 import { ContactoComponent } from './contacto/contacto.component';
-
+import { LugaresService } from './services/lugares.service';
 
 const appRoutes: Routes = [
   {path: '', component: LugaresComponent},
@@ -44,7 +44,9 @@ const appRoutes: Routes = [
   ],
 
   //se declaran los servicios propios que se van a necesitar en la aplicacion
-  providers: [],
+  providers: [
+    LugaresService
+  ],
 
   //se declara con que componente se va a iniciar
   bootstrap: [AppComponent]
