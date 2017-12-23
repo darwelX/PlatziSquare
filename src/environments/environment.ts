45ec47adc1daf642681bd958b5a7cb868e465784
Darwel Quintero <darwel34@gmail.com>
2017-12-23 14:06:37 -0430

    se agrego el objeto de configuracion de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBGbYjxUgkTnS8yigLzw8bBUcsLXUhLH-M",
+    authDomain: "platzisquare-1513296720077.firebaseapp.com",
+    databaseURL: "https://platzisquare-1513296720077.firebaseio.com",
+    storageBucket: "platzisquare-1513296720077.appspot.com",
+    messagingSenderId: "824687402456"
+  }
 };
