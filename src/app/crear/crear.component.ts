c7054e00d2f4212563214ae6263d53e09cc48ab9
Darwel Quintero <darwel34@gmail.com>
2017-12-25 16:56:21 -0430

    se añadieron las variables de geo posiscionamiento al crear un lugar y se añadio la logica para que el componente sea reutilizado pra la edicion de registros

--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -1,5 +1,6 @@
 import { LugaresService } from '../services/lugares.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import swal from 'sweetalert';
 
 @Component({
@@ -9,18 +10,40 @@ import swal from 'sweetalert';
 })
 export class CrearComponent implements OnInit {
   lugar: any = {};
+  action: any = null;
 
-  constructor(private lugaresServices: LugaresService) { }
+  constructor(private lugaresServices: LugaresService, private route: ActivatedRoute) { 
+    // debugger;
+    this.action = this.route.snapshot.queryParams['action'];
+    if(this.action == 'edit'){
+      this.buscar()
+    }
+  }
 
   ngOnInit() {
   }
 
+  buscar(){
+    let id = this.route.snapshot.params['id'];
+    this.lugaresServices.getLugar(id)
+      .valueChanges().subscribe(lugar => {
+          this.lugar = lugar;
+        })
+  }
+  
   crear(){
-    this.lugar.id = Date.now();
-    this.lugar.activo = true;
-    this.lugaresServices.guardarLugar(this.lugar);
-    swal('Registro guardado', 'Ok', 'success');
-    this.lugar = {};
+    let direccion = this.lugar.calle+','+this.lugar.ciudad+','+this.lugar.pais;
+    this.lugaresServices.obtenerGeoData(direccion)
+    .subscribe((result)=>{
+       debugger;
+       this.lugar.lg = result.json().results[0].geometry.location.lng;
+       this.lugar.lt = result.json().results[0].geometry.location.lat;
+       this.lugar.id = Date.now();
+       this.lugar.activo = true;
+       this.lugaresServices.guardarLugar(this.lugar);
+       swal('Registro guardado', 'Operacion Exitosa', 'success');
+       this.lugar = {};      
+    });
     // console.log(this.lugar);
   }
 }
