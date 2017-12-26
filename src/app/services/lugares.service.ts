f2fac4cd035d9f33b7b06015f4179a5e8466329d
Darwel Quintero <darwel34@gmail.com>
2017-12-26 04:42:58 -0430

    se añadio la logica para utilizar llamados http

--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { AngularFireDatabase } from'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { Headers } from '@angular/http';
 
 @Injectable()
 export class LugaresService {
   lugares: any = [];
+  END_POINT = 'https://platzisquare-1513296720077.firebaseio.com';
 
   public getLugares(){
     return this.afDB.list('lugares/');
@@ -17,6 +19,8 @@ export class LugaresService {
 
   public guardarLugar(lugar){
     this.afDB.database.ref('lugares/'+lugar.id).set(lugar);
+    let headers = new Headers({"Content-type": "application/json"});
+    return this.http.post(this.END_POINT+'/lugares.json', lugar, {headers: headers});
   }
 
   public updateLugar(lugar){
