8fdecd5784306d6f798d1b333f7b99ce1c292f66
Darwel Quintero <darwel34@gmail.com>
2017-12-27 08:30:57 -0430

    se agrego el idioma español

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,6 +1,10 @@
 import { LugaresService } from '../services/lugares.service';
 import { Component, OnInit } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+import { registerLocaleData } from '@angular/common';
 import  swal  from 'sweetalert';
+import { LOCALE_ID } from '@angular/core';
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
@@ -10,7 +14,9 @@ export class LugaresComponent implements OnInit {
   lat:number = 8.215178;
   lng:number = -72.257524;
   lugares: any = null;
+  
   constructor(private lugaresServices: LugaresService) { 
+    registerLocaleData(localeEs, 'es', localeEsExtra);
     this.lugaresServices.getLugares()
     .subscribe(lugares => {
       if(lugares.status == 200){
