db95a1d2413e10e947fa483f8927140165e72a32
Darwel Quintero <darwel34@gmail.com>
2017-12-28 09:23:39 -0430

    se agrego sintaxis de animacion

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -5,16 +5,54 @@ import localeEsExtra from '@angular/common/locales/extra/es';
 import { registerLocaleData } from '@angular/common';
 import  swal  from 'sweetalert';
 import { LOCALE_ID } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.css']
+  styleUrls: ['./lugares.component.css'],
+  animations: [
+    trigger('contenedorAnimable', [
+      state('inicial', style({
+        opacity: 0
+      })),
+      state('final', style({
+        opacity: 1,      
+      })),
+      transition('inicial => final',animate(5000)),
+      transition('final => inicial',animate(1000))
+    ]),
+    trigger('contenedorStar', [
+      state('small', style({
+        opacity: 0,
+        transform: 'scale(.5)'
+      })),
+      state('big', style({
+        opacity: 1,
+        transform: 'scale(1)'      
+      })),
+      transition('small => big',animate(3000)),
+      transition('big => small',animate(500))
+    ]),   
+  ]
 })
 export class LugaresComponent implements OnInit {
   lat:number = 8.215178;
   lng:number = -72.257524;
   lugares: any = null;
-  
+  // state='inicial';
+
+  // animar(){
+  //   this.state = (this.state=='inicial') ? 'final' : 'inicial';
+  // }
+
+  // animacionIniciada(event){
+  //   console.log('iniciada',event);
+  // }
+
+  // animacionTerminada(event){
+  //   this.animateStateStar = 'big';
+  //   console.log('terminada', event);
+  // }
   constructor(private lugaresServices: LugaresService) { 
     registerLocaleData(localeEs, 'es', localeEsExtra);
     this.lugaresServices.getLugares()
@@ -23,6 +61,7 @@ export class LugaresComponent implements OnInit {
         let obj = lugares.json();
         this.lugares= Object.keys(obj).map( (key) => { return obj[key]; });
         //this.lugares = Object.keys(this.lugares).map((key) => this.lugares[key]);
+        // this.state='final';
       }else{
         swal('A ocurrido un Error', 'Codigo '+lugares.status, 'error');
       }
