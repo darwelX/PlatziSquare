8d90936ed31646ec700ab0270bec2175ad8cd8f7
Darwel Quintero <darwel34@gmail.com>
2018-01-01 16:53:51 -0430

    se reseteo el test de detalle component

--- src/app/detalle/detalle.component.spec.ts
+++ src/app/detalle/detalle.component.spec.ts
@@ -1,25 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetalleComponent } from './detalle.component';
+import { LugaresService } from '../services/lugares.service';
+import { LugaresComponent } from '../lugares/lugares.component';
 
 describe('DetalleComponent', () => {
-  let component: DetalleComponent;
-  let fixture: ComponentFixture<DetalleComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DetalleComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DetalleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
