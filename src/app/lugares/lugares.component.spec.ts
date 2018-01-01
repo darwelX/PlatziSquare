dc5f27ac8df60834a8a10fb243792310c93b468f
Darwel Quintero <darwel34@gmail.com>
2018-01-01 16:54:39 -0430

    se creo el test para lugares

--- src/app/lugares/lugares.component.spec.ts
+++ src/app/lugares/lugares.component.spec.ts
@@ -1,25 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AgmCoreModule } from '@agm/core';
 import { LugaresComponent } from './lugares.component';
+import { LugaresService } from '../services/lugares.service';
 
 describe('LugaresComponent', () => {
-  let component: LugaresComponent;
-  let fixture: ComponentFixture<LugaresComponent>;
+  let fixture;
+  let lugaresServices: LugaresService;
+  let app;
+  let lugares: any=null;
 
-  beforeEach(async(() => {
+  beforeEach(()=>{
     TestBed.configureTestingModule({
-      declarations: [ LugaresComponent ]
+      declarations: [LugaresComponent],
+      imports: [
+        AgmCoreModule.forRoot({
+          apiKey: 'AIzaSyD_WGF3n-7hURPte4-4LXr7uSEHZJXXAKg'
+        })
+      ]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(LugaresComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    app = fixture.debugElement.componentInstance;
+    lugaresServices = fixture.debugElement.injector.get(LugaresService);
+  })
+  it('detalles', ()=>{
+    lugaresServices.getLugares()
+    .subscribe(lugares => {
+      if(lugares.status == 200){
+        let obj = lugares.json();
+        this.lugares= Object.keys(obj).map( (key) => { return obj[key]; });
+      }
+    });
+    expect(app.lugares).toEqual(this.lugares);
+  })
 });
