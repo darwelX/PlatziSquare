e2fa863596eba2552191b8f04167c9836721b5f7
Darwel Quintero <darwel34@gmail.com>
2018-01-02 15:49:11 -0430

    se hizo un manejo de errores

--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -3,20 +3,18 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import swal from 'sweetalert';
 import { Router } from '@angular/router';
-
 @Injectable()
 export class AutorizacionService {
 
   constructor(private angularFireAuth: AngularFireAuth, private afDB: AngularFireDatabase, private router: Router) { 
-
   }
+
   public login = (username, password) => {
     this.angularFireAuth.auth.signInWithEmailAndPassword(username, password)
       .then((response)=>{
         this.router.navigate(['lugares']);
       })
       .catch((error)=>{
-        console.log(error);
         swal('Ha ocurrido un Error', error.message, 'error')
       })
   }
@@ -28,14 +26,18 @@ export class AutorizacionService {
       this.afDB.database.ref('Users').child(id).set({
         activo: 1
       })
-      swal('Operación exitosa','Registro guardado con exito','success');
-      // console.log(typeof(response));
       callback(null, JSON.parse(JSON.stringify(response)));
     })
     .catch((error)=>{
-      // console.log(error);
-      swal('A ocurrido un Error '+ error.statusText, 'Codigo '+error.status, 'error');
-      callback(new Error(`Se produjo un error al realizar el request ${error.status}`))
+      callback(new Error(`Error - ( ${error.message} )`))
     });
+  }
+
+  public logout = ()=>{
+    this.angularFireAuth.auth.signOut();
+  }
+
+  public isLogged = () => {
+    return this.angularFireAuth.authState;
   }
 }
