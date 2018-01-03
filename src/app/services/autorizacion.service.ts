f86195a8871b8c8c46405831d848bc63d738599f
Darwel Quintero <darwel34@gmail.com>
2018-01-03 07:18:07 -0430

    se agrego el metodo de autenticacion con facebook

--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -3,12 +3,27 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import swal from 'sweetalert';
 import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+
 @Injectable()
 export class AutorizacionService {
 
   constructor(private angularFireAuth: AngularFireAuth, private afDB: AngularFireDatabase, private router: Router) { 
+    this.isLogged();
   }
 
+  public loggingFacebook = (callback) => {
+    this.angularFireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then( (result)=>{
+        // console.log(result);
+        return callback(null, JSON.parse(JSON.stringify(result)));
+      })
+      .catch((error)=>{
+        // console.log(error);
+        return callback(new Error(`Error - ( ${error.message} )`));
+      });
+
+  }
   public login = (username, password) => {
     this.angularFireAuth.auth.signInWithEmailAndPassword(username, password)
       .then((response)=>{
