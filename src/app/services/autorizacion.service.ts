a74de8a694ba1f1c459892e56dd54118ab94e85b
Darwel Quintero <darwel34@gmail.com>
2018-01-06 07:43:24 -0430

    se creo el loggin con twiter

--- src/app/services/autorizacion.service.ts
+++ src/app/services/autorizacion.service.ts
@@ -5,11 +5,22 @@ import swal from 'sweetalert';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
 
+
 @Injectable()
 export class AutorizacionService {
 
   constructor(private angularFireAuth: AngularFireAuth, private afDB: AngularFireDatabase, private router: Router) { 
     this.isLogged();
+  }
+
+  public loggingTwitter = (callback)=>{
+    this.angularFireAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
+      .then( (result)=>{
+        return callback(null, JSON.parse(JSON.stringify(result)));
+      })
+      .catch((error)=>{
+        return callback(new Error(`Error - ( ${error.message} )`));
+      });
   }
 
   public loggingFacebook = (callback) => {
