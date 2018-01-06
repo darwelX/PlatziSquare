293cc3d39f33445771995db55582cdbab4957f38
Darwel Quintero <darwel34@gmail.com>
2018-01-06 07:51:38 -0430

    se agrego la variable spiner para personalizar y se customizo el nombre de usuario a mostrar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AutorizacionService } from './services/autorizacion.service';
 import { Router } from '@angular/router';
+import { Spinkit } from 'ng-http-loader/spinkits';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,15 @@ export class AppComponent {
   title = 'PlatziSquare';
   loggedIn: boolean = false;
   userName: string  = ''
+  public spinkit = Spinkit;
+  
   constructor(private autorizacionService: AutorizacionService, private router: Router){
     this.autorizacionService.isLogged()
       .subscribe((result)=>{
         if(result && result.uid){
           this.loggedIn = true;
           let email = result.email;
-          this.userName = email.substring(0,email.lastIndexOf('@'));
+          this.userName = (result.email)?email.substring(0,email.lastIndexOf('@')): result.displayName;
           this.router.navigate(['']);
         }else{
           this.loggedIn = false;
